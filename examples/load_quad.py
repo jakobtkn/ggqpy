80c9df4961ce33de12fafdf237c70c3dc488736e
jakobtkn <s194325@student.dtu.dk>
2024-03-23 15:20:12 +0100

    Read quadrature from file example script

---
+++ examples/load_quad.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("."))
+from ggqpy import *
+
+def main():
+    quad = Quadrature.load_from_file("quads/out.quad")
+    print(np.sum(quad.w))
+    return
+
+
+
+if __name__=="__main__":
+    main()
