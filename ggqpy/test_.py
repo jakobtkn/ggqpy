c1317b0b3406df3a519c49a8b363902be076cd98
jakobtkn <s194325@student.dtu.dk>
2024-03-23 16:54:50 +0100

    Added end to end test for polynomial + 1/x integration

--- ggqpy/test_.py
+++ ggqpy/test_.py
@@ -1,6 +1,8 @@
 import numpy as np
-from ggqpy.optimize import *
+import sympy 
 from pytest import approx
+
+from ggqpy import *
 
 def test_sherman_morrison():
     A = np.random.random(size=(5,5))
@@ -12,3 +14,14 @@ def test_sherman_morrison():
 
     sherman_morrison(Ainv,u,v)
     assert(np.linalg.norm(Ainv - np.linalg.inv(A + np.outer(u,v))) == approx(0))
+
+def test_end_to_end_pylonimal():
+    I = Interval(1e-9,1)
+    F = FunctionFamilySymbolic.polynomials_and_singularity(I)
+    x,w = generalized_gaussian_quadrature(F)
+    
+    f_symbolic, f_lambda = F.draw_function()
+
+    integral_numeric = f_lambda(x)@w
+    integral_analytic = F.integral(f_symbolic)
+    assert(integral_numeric == approx(integral_analytic))
