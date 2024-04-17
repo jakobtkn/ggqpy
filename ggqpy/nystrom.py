ff1b067646bfca00cdb6ee38902350e531368553
jakobtkn <s194325@student.dtu.dk>
2024-04-17 14:12:32 +0200

    fixed bug in convert to standard triangle

--- ggqpy/nystrom.py
+++ ggqpy/nystrom.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import numpy as np
+from numpy.typing import NDArray
 from itertools import pairwise
 from ggqpy.utils import *
 
@@ -8,7 +9,7 @@ def ensure_conformal_mapping(jacobian, x0):
     """
     Returns B such that
     Ax = Bx + x0
-    ensures \tilde{\rho}=\rho\circ A such that A(0,0) = x and the Jacobian of \tilde{\rho} is orthogonal at the point (0,0).
+    ensures tilde{rho}=rho.A such that A(0,0) = x and the Jacobian of tilde{rho} is orthogonal at the point (0,0).
 
     Parameters
     ----------
@@ -24,18 +25,13 @@ def ensure_conformal_mapping(jacobian, x0):
 
     _, s, Vh = np.linalg.svd(J, full_matrices=False)
     V = Vh.T
-    S = np.diag(s ** (-1))
+    S = np.diag(s)
     Sinv = np.diag(s ** (-1))
 
     B = V @ Sinv
     Binv = S @ Vh
 
-    b = x0
-
-    affine = lambda x: B @ x + b
-    affine_inverse = lambda x: Binv @ (x - b)
-
-    return affine, affine_inverse
+    return B, Binv
 
 
 def standard_radial_triangle_transform(a, b):
@@ -45,9 +41,9 @@ def standard_radial_triangle_transform(a, b):
 
     Triangle is assumed to be on the form.
     b
-    |\
-    | \
-    |  \
+    |\\
+    | \\
+    |  \\
     0---a
 
     Parameters
@@ -76,8 +72,8 @@ def standard_radial_triangle_transform(a, b):
     r = np.linalg.norm(u)
     theta = np.arctan2(u[1], u[0])
     c, s = np.cos(theta), np.sin(theta)
-    A = np.array([[c, -s], [s, c]]) / r
-    Ainv = np.array([[c, s], [-s, c]]) * r
+    A = np.array([[c, s], [-s, c]]) / r
+    Ainv = np.array([[c, -s], [s, c]]) * r
     det = (1 / r) ** 2
 
     x = A @ v
@@ -85,8 +81,8 @@ def standard_radial_triangle_transform(a, b):
     scale = np.linalg.norm(x)
 
     if angle < 0.0:
-        A = np.array([[c, s], [-s, c]]) / r
-        Ainv = np.array([[c, -s], [s, c]]) * r
+        Ainv[:,1] = -Ainv[:,1]
+        A[1,:] = -A[1,:]
         angle = -angle
 
     return scale, angle, A, Ainv, det
@@ -123,12 +119,12 @@ class Triangle:
     Triangle class. Corners are presented as numpy arrays x,y where
 
     (x2,y2)
-       |\
-       | \
-       |  \
-       |   \      
-       |    \      
-       |     \     
+       |\\
+       | \\
+       |  \\
+       |   \\      
+       |    \\      
+       |     \\     
     (x0,y0)--(x1,y1)
     """
 
@@ -146,13 +142,31 @@ class Triangle:
     def __eq__(self, other: Triangle) -> bool:
         return len(set(self.vertices + other.vertices)) == 3
 
-def load_ggq_quad(r0,theta0):
+    def __iter__(self):
+        for v in self.vertices:
+            yield v
+
+    def is_in(self, P):
+        A, B, C = self.vertices
+
+        denominator = (B[1] - C[1]) * (A[0] - C[0]) + (C[0] - B[0]) * (A[1] - C[1])
+        a = (
+            (B[1] - C[1]) * (P[0] - C[0]) + (C[0] - B[0]) * (P[1] - C[1])
+        ) / denominator
+        b = (
+            (C[1] - A[1]) * (P[0] - C[0]) + (A[0] - C[0]) * (P[1] - C[1])
+        ) / denominator
+        c = 1 - a - b
+
+        return a >= 0 and b >= 0 and c >= 0
+
+
+def load_ggq_quad(r0, theta0):
     ###!!! pick correct ###
     order = 4
-    quad = Quadrature.load_from_file(
-        f"quads/nystrom.{16}.{order}.quad"
-    )
+    quad = Quadrature.load_from_file(f"quads/nystrom.{16}.{order}.quad")
     return quad, order
+
 
 def quad_on_standard_triangle(r0, theta0):
     gamma = (
@@ -166,23 +180,44 @@ def quad_on_standard_triangle(r0, theta0):
     theta_global = list()
     r_global = list()
 
-    for (u,w) in [*ggq]:
+    for u, w in [*ggq]:
         gammau = gamma(u)
-        gl = Quadrature.gauss_legendre_on_interval(order, Interval(0,gammau))
-        
-        w_global.append(w*gl.w*theta0)
+        gl = Quadrature.gauss_legendre_on_interval(order, Interval(0, gammau))
+
+        w_global.append(w * gl.w * theta0)
         r_global.append(gl.x)
-        theta_global.append(np.full_like(gl.x, u*theta0))
+        theta_global.append(np.full_like(gl.x, u * theta0))
 
     r = np.concatenate(r_global)
     theta = np.concatenate(theta_global)
     w = np.concatenate(w_global)
-    return r,theta,w
+    return r, theta, w
 
-def singular_integral_quad(rho, drho, x0, simplex):
-    B, affine, inverse_affine = ensure_conformal_mapping(drho, x0)  
-    R = Rectangle(inverse_affine(*simplex))
 
-    for T in [*R.split_into_triangles_around_point(x0)]:
-        scale, angle, A, Ainv, det = standard_radial_triangle_transform(T.vertices[1], T.vertices[2])
-        T0 = Triangle((0,0),(1,0), (scale*np.cos(angle), scale*np.sin(angle)))
+def singular_integral_quad(drho, x0, simplex):
+    B, Binv = ensure_conformal_mapping(drho, x0)
+    R = Rectangle(*[Binv @ (np.array(v) - x0) for v in iter(simplex)])
+
+    x_list = list()
+    y_list = list()
+    w_list = list()
+
+    for T in [*R.split_into_triangles_around_point((0,0))]:
+        scale, angle, A, Ainv, det = standard_radial_triangle_transform(
+            T.vertices[1], T.vertices[2]
+        )
+        r, theta, w = quad_on_standard_triangle(scale, angle)
+        x_local = np.cos(theta) * r
+        y_local = np.sin(theta) * r
+
+        v = np.row_stack([x_local, y_local])
+        v = B @ (Ainv @ v) + x0[:, np.newaxis]
+
+        x_list.append(v[0, :])
+        y_list.append(v[1, :])
+        w_list.append(w)
+
+    x = np.concatenate(x_list)
+    y = np.concatenate(y_list)
+
+    return x, y, w
--- ggqpy/test_nystrom.py
+++ ggqpy/test_nystrom.py
@@ -12,11 +12,14 @@ def test_radial_transform():
     alpha, phi, A, Ainv, det = standard_radial_triangle_transform(a, b)
     assert alpha == approx(1 / 2.0)
     assert phi == approx(np.pi / 2.0)
+
+    np.testing.assert_allclose(A@Ainv, np.identity(2))
     np.testing.assert_allclose(A @ b, np.array([1, 0]), atol=1e-15)
     np.testing.assert_allclose(
         A @ a, np.array([alpha * np.cos(phi), alpha * np.sin(phi)]), atol=1e-15
     )
     np.testing.assert_allclose(Ainv @ np.array([1, 0]), b, atol=1e-15)
+    np.testing.assert_allclose(Ainv @ np.array([alpha * np.cos(phi), alpha * np.sin(phi)]), a, atol=1e-15)
 
     a = (-1.0, 0)
     b = (0, 2.0)
@@ -29,12 +32,27 @@ def test_radial_transform():
     alpha, phi, A, Ainv, det = standard_radial_triangle_transform(a, b)
     assert alpha == approx(1 / np.pi)
     assert phi == approx(np.pi / 2.0)
+    
 
     a = (-1.0, 1.0)
     b = (0, 1.0)
     alpha, phi, A, Ainv, det = standard_radial_triangle_transform(a, b)
     assert alpha == approx(1 / np.sqrt(2))
-    assert phi == approx(3 * np.pi / 4.0)
+    assert phi == approx(np.pi / 4.0)
+
+
+
+    a = (-9.0, 3)
+    b = (-20, 2.0)
+    alpha, phi, A, Ainv, det = standard_radial_triangle_transform(a, b)
+
+    np.testing.assert_allclose(A@Ainv, np.identity(2), atol=1e-8)
+    np.testing.assert_allclose(A @ b, np.array([1, 0]), atol=1e-15)
+    np.testing.assert_allclose(
+        A @ a, np.array([alpha * np.cos(phi), alpha * np.sin(phi)]), atol=1e-15
+    )
+    np.testing.assert_allclose(Ainv @ np.array([1, 0]), b, atol=1e-15)
+    np.testing.assert_allclose(Ainv @ np.array([alpha * np.cos(phi), alpha * np.sin(phi)]), a, atol=1e-15)
 
 
 def test_conformal_mapping():
@@ -42,10 +60,10 @@ def test_conformal_mapping():
     drho = lambda x: np.array([[0, 0], [1, 0], [0, 2 * x[1]]])
     x0 = np.array([2.0, 1.0])
 
-    affine, affine_inverse = ensure_conformal_mapping(drho, x0)
+    B, Binv = ensure_conformal_mapping(drho, x0)
 
-    np.testing.assert_allclose(affine(np.array([0, 0])), x0)
-
+    np.testing.assert_allclose(B@(np.array([0, 0])) + x0, x0)
+    np.testing.assert_allclose(B@Binv, np.identity(2))
 
 def test_geometry():
     R = Rectangle((-1, -1), (1, -1), (1, 1), (-1, 1))
@@ -60,7 +78,65 @@ from examples.experiment_triangle import analytic_integral
 
 def test_quad_on_standard_triangle():
     r,theta,w = quad_on_standard_triangle(0.5, np.pi/2)
-    print(np.shape(r), np.shape(theta), np.shape(w))
     assert len(r) == len(theta) == len(w)
     f = lambda r, theta: np.cos(2 * theta)
     assert f(r,theta)@w == approx(analytic_integral(0.5))
+
+def test_singular_integral(plt):
+    drho = lambda x: np.array([[0, 0], [1, 0], [0, 2 * x[1]]])
+    x0 = np.array([0.5, 0.5])
+    simplex = Rectangle((-1,-1),(1,-1),(1,1),(-1,1))
+    x,y,w = singular_integral_quad(drho,x0,simplex)
+    plt.figure()
+    plt.scatter(x,y,marker="x")
+    plt.scatter(x0[0],x0[1],c="r")
+    plt.xlim(-1,1)
+    plt.ylim(-1,1)
+
+def test_node_placement(plt):
+    drho = lambda x: np.array([[0, 0], [1, 0], [0, 2 * x[1]]])
+    x0 = np.array([0.5, 0.5])
+    simplex = Rectangle((-1,-1),(1,-1),(1,1),(-1,1))
+    
+    B, Binv = ensure_conformal_mapping(drho, x0)
+    R = Rectangle(*[Binv @ (np.array(v) - x0) for v in iter(simplex)])
+
+    x_list = list()
+    y_list = list()
+    w_list = list()
+    T, T2, T3, T4 = [*R.split_into_triangles_around_point(np.array([0,0]))]
+
+    scale, angle, A, Ainv, det = standard_radial_triangle_transform(
+        T.vertices[1], T.vertices[2]
+    )
+    T0 = Triangle((0,0),(1,0),(scale*np.cos(angle), scale*np.sin(angle)))
+    r, theta, w = quad_on_standard_triangle(scale, angle)
+    x_local = np.cos(theta) * r
+    y_local = np.sin(theta) * r
+
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.title("T0")
+    for x,y in [*T0]:
+        plt.scatter(x,y,c="r")
+    for x,y in zip(x_local,y_local):
+        plt.scatter(x,y,c="b")
+        assert T0.is_in((x,y))
+
+    v = np.row_stack([x_local, y_local])
+    np.testing.assert_allclose(v[0,:], x_local)
+    np.testing.assert_allclose(v[1,:], y_local)
+    print(v)
+    plt.subplot(1, 2, 2)
+    plt.title("T")
+    for x,y in [*T]:
+        plt.scatter(x,y,c="r")
+    # plt.scatter(np.hsplit(np.row_stack([*T]),2))
+    for x,y in zip((Ainv@v)[0, :],(Ainv@v)[1, :]):
+        plt.scatter(x,y,c="b")
+        # assert T.is_in((x,y))
+        
+
+    v = B @ (Ainv @ v) + x0[:, np.newaxis]
+
