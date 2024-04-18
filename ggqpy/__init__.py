5e422e559efa248c658e760605f2a2343976b76a
jakobtkn <s194325@student.dtu.dk>
2024-04-18 11:00:20 +0200

    reformatting

--- ggqpy/__init__.py
+++ ggqpy/__init__.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy as sp
 from ggqpy.discretize import *
 from ggqpy.optimize import *
+from ggqpy.quad import Quadrature
 from ggqpy.utils import *
 from ggqpy.nystrom import *
 
@@ -49,12 +50,12 @@ def construct_Chebyshev_quadratures(eval_points: tuple, w, U):
 
 def generalized_gaussian_quadrature(
     function_family: FunctionFamily,
-    min_length=1e-6,
-    eps_disc=1e-13,
-    eps_comp=1e-13,
-    eps_quad=1e-7,
-    interpolation_degree=30,
-    detailed_output=None,
+    min_length: float = 1e-6,
+    eps_disc: float = 1e-13,
+    eps_comp: float = 1e-13,
+    eps_quad: float = 1e-7,
+    interpolation_degree: int = 30,
+    full: bool = None,
 ):
     """
 
@@ -69,25 +70,30 @@ def generalized_gaussian_quadrature(
     vprint(
         f"Function family consists of {len(function_family.functions_lambdas)} functions"
     )
+
+    ## Discretize
     discretizer = Discretizer(eps_disc, min_length, interpolation_degree)
     x_disc, w_disc = discretizer.adaptive_discretization(function_family)
     vprint(
         f"Adaptive discretization divided the domain into {len(discretizer.intervals)} subintervals"
     )
 
+    ## Compress functions
     U_disc, rank = compress_sequence_of_functions(
         function_family.functions_lambdas, x_disc, w_disc, eps_comp
     )
-
     vprint(f"Determined numerical rank to be {rank}")
 
+    ## Construct Chebyshev quadrature
     (x_cheb,), w_cheb = construct_Chebyshev_quadratures((x_disc,), w_disc, U_disc)
     r = U_disc.T @ w_disc
+
+    ## Point by point reduction.
     U_family = discretizer.interpolate_piecewise_legendre(U_disc)
     optimizer = QuadOptimizer(U_family, r)
     x, w = optimizer.reduce_quadrature(x_cheb, w_cheb, eps_quad)
 
-    if detailed_output:
+    if full:
         return Quadrature(x, w), Quadrature(x_disc, w_disc), Quadrature(x_cheb, w_cheb)
 
     return x, w
