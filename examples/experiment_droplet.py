Fixed sign error in droplet parametrization

examples/experiment_droplet.py
@@ -23,7 +23,7 @@ def kernel(x0, y0, s, t):
     n = normal(s, t)
     dist = np.linalg.norm(q - p, axis=0)
     return (
-        (np.sum(n * (p - q), axis=0))
+        (np.sum(n * (q - p), axis=0))
         / dist**3
         * np.exp(1j * k * dist)
         * (1.0 - 1j * k * dist)
@@ -48,14 +48,14 @@ h, h_grad = param.h_and_hgrad()
 
 def dh(s, t):
     x, y, z = rho(s, t)
-    return np.sum(-normal(s,t) * h_grad(x, y, z), axis=0)
+    return np.sum(normal(s,t) * h_grad(x, y, z), axis=0)
 
 
 f = dh(ss, tt) * np.sqrt(ww)
 A = -0.5 * np.identity(M * N) + (4.0 * np.pi) ** (-1) * A
 q = np.linalg.solve(A, f)
 
-p0 = np.array([10, 0, 0])
+p0 = np.array([-10, 5, 0])
 
 
 def single_layer(s, t):

ggqpy/parametrization.py
@@ -12,7 +12,7 @@ def _de_dimension(_f):  ## Correct for fixed 2d arrays in sympy
 
 
 class Parametrization:
-    def __init__(self, s, t, x, y, z):
+    def __init__(self, s, t, x, y, z, flip_normal=False):
         self.s = s
         self.t = t
         self.x = x
@@ -22,13 +22,17 @@ class Parametrization:
         self.rho = sp.Matrix([x, y, z])
         self.variables = sp.Matrix([s, t])
         self.drho = self.rho.jacobian(self.variables)
-        
+
         cross_product = self.drho[:, 0].cross(self.drho[:, 1])
         self.jacobian = sp.simplify(cross_product.norm())
-        self.normal = -cross_product.normalized()
+
+        if flip_normal:
+            self.normal = sp.simplify(cross_product.normalized())
+        else:
+            self.normal = sp.simplify(-cross_product.normalized())
 
     def get_lambdas(self):
-        s,t = self.s,self.t
+        s, t = self.s, self.t
         _rho = sp.lambdify((s, t), self.rho, modules="numpy")
         _drho = sp.lambdify((s, t), self.drho, modules="numpy")
         _jacobian = sp.lambdify((s, t), self.jacobian, modules="numpy")
@@ -44,7 +48,7 @@ class Parametrization:
         x = sp.cos(s) * sp.sin(t)
         y = sp.sin(s) * sp.sin(t)
         z = sp.cos(t)
-        return cls(s,t,x,y,z)
+        return cls(s, t, x, y, z, flip_normal=False)
 
     @classmethod
     def droplet(cls):
@@ -54,20 +58,24 @@ class Parametrization:
         x = 2 * sp.sin(t / 2)
         y = sp.cos(s) * sp.sin(t)
         z = sp.sin(s) * sp.sin(t)
-        return cls(s,t,x,y,z)
-    
-    def h_and_hgrad(self, p0 = [1,0,0]):
+
+        return cls(s, t, x, y, z, flip_normal=True)
+
+    def h_and_hgrad(self, p0=[1, 0, 0]):
         p0 = sp.Matrix(p0)
-        x,y,z = sp.symbols("x y z", real=True)
+        x, y, z = sp.symbols("x y z", real=True)
 
-        h = sp.exp(sp.I * (sp.Matrix([x,y,z]) - p0).norm()) / (sp.Matrix([x,y,z]) - p0).norm()
-        h_grad = sp.simplify(sp.Matrix([h.diff(var) for var in [x,y,z]]))
+        h = (
+            sp.exp(sp.I * (sp.Matrix([x, y, z]) - p0).norm())
+            / (sp.Matrix([x, y, z]) - p0).norm()
+        )
+        h_grad = sp.simplify(sp.Matrix([h.diff(var) for var in [x, y, z]]))
 
-        _h = sp.lambdify((x,y,z), h, modules="numpy")        
-        _h_grad = sp.lambdify((x,y,z), h_grad, modules="numpy")
+        _h = sp.lambdify((x, y, z), h, modules="numpy")
+        _h_grad = sp.lambdify((x, y, z), h_grad, modules="numpy")
         for f in [_h, _h_grad]:
             yield _de_dimension(f)
-    
+
     def print(self):
         print("drho:")
         print(drho)
@@ -75,40 +83,6 @@ class Parametrization:
         print(jacobian)
         print("normal:")
         print(normal)
-
-def parametrize_sphere():
-    s = sp.symbols("s", real=True, domain=(0, 2 * sp.pi))
-    t = sp.symbols("t", real=True, domain=(0, sp.pi))
-
-    x = sp.cos(s) * sp.sin(t)
-    y = sp.sin(s) * sp.sin(t)
-    z = sp.cos(t)
-
-    rho = sp.Matrix([x, y, z])
-    return determine_parametrization(rho, s, t)
-
-def determine_parametrization(rho, s, t):
-    variables = sp.Matrix([s, t])
-    drho = rho.jacobian(variables)
-    cross_product = drho[:, 0].cross(drho[:, 1])
-    jacobian = sp.simplify(cross_product.norm())
-    normal = -cross_product.normalized()
-
-    if verbose:
-        print("drho:")
-        print(drho)
-        print("Jacobian:")
-        print(jacobian)
-        print("normal:")
-        print(normal)
-
-    _rho = sp.lambdify((s, t), rho, modules="numpy")
-    _drho = sp.lambdify((s, t), drho, modules="numpy")
-    _jacobian = sp.lambdify((s, t), jacobian, modules="numpy")
-    _normal = sp.lambdify((s, t), normal, modules="numpy")
-
-    for f in [_rho, _drho, _jacobian, _normal]:
-        yield _de_dimension(f)
 
 
 if __name__ == "__main__":
@@ -119,6 +93,6 @@ if __name__ == "__main__":
     # print(h(1,1,1))
     s = np.arange(10)
     t = np.arange(10)
-    x,y,z = rho(s,t)
-    print(normal(0,1))
-    print(h_grad(*rho(0,1)))
+    x, y, z = rho(s, t)
+    print(normal(0, 1))
+    print(h_grad(*rho(0, 1)))