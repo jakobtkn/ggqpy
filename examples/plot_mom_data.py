8e86479280d668115e47c15fd6b425486163fd25
jakobtkn <jakobtkn@gmail.com>
2024-06-07 10:57:13 +0200

    tikzplotlib

--- examples/plot_mom_data.py
+++ examples/plot_mom_data.py
@@ -1,12 +1,16 @@
 from tester import Tester, load_tester
+import itertools
 import pickle
 import matplotlib.pyplot as plt
+
 import sys,os
 sys.path.append(os.path.abspath("."))
 
 
 
 tester_names = ["naive","duffy","ggq_precomputed_4","ggq_precomputed_8","ggq_precomputed_16","ggq_exact_triangle","ticra"]
+display_names = ["Gauss-Legendre","Duffy","GGQ, $n=4$","GGQ, $n=8$","GGQ, $n=16$","GGQ, $n=4$, Exact triangle","TICRA's method"]
+
 def plot_data(folder):
     testers = list[Tester]()
     
@@ -14,15 +18,20 @@ def plot_data(folder):
         tester = load_tester(folder, name)
         testers.append(tester)
 
+    marker = itertools.cycle(('s', '+', '.', 'o', '*','^','v')) 
     plt.figure()
-    for tester in testers:
-        plt.loglog(tester.num_nodes, tester.rel_error, "-*", label=tester.name)
+    for tester, display_name in zip(testers,display_names):
+        plt.loglog(tester.num_nodes, tester.rel_error, "-*", label=display_name, marker = next(marker))
 
     plt.legend()
     plt.xlabel("Number of quadrature nodes")
     plt.xlim(1e3,1e6)
     plt.ylabel("Relative error")
-    plt.savefig(f"output/mom.{folder}.pdf")
+    plt.grid(True)
+    # plt.savefig(f"output/mom.{folder}.pdf")
+
+    import tikzplotlib
+    tikzplotlib.save(f"output/{folder}.tex")
 
 
 if __name__ == "__main__":
